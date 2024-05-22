d355d4df6e46fdca59f0093d5cc68118056b003b
Bishal Kar <134054921+Bishal01234@users.noreply.github.com>
2024-05-22 09:30:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("Bishal")
+
+let name = "Bishal"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigintn =>
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof 123);
+console.log(typeof null);
+console.log(typeof undefined);
+
