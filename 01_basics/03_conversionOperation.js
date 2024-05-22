61394fcbb645372f133bac5b8e08fc35e3b4060b
Bishal Kar <134054921+Bishal01234@users.noreply.github.com>
2024-05-22 10:18:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = 33
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Bishal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Bishal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true;   0 => false
+// "" => false; "Bishal" => true
+
+let someNum = 43
+
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
