9faff7065b181b879d5b50628d6da81881a855a9
Bishal Kar <134054921+Bishal01234@users.noreply.github.com>
2024-05-31 17:42:08 +0000

    done

---
+++ 05_iterations/five.js
@@ -0,0 +1,15 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item)
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val)
+// })
+
+function printMe(item){
+    console.log(item)
+}
+
+coding.forEach(printMe)
